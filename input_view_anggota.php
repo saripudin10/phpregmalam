92a4c7d8ddfe6c33388c3864879e2c3a27529309
saripudin10 <saripudin487@gmail.com>
2021-11-02 09:25:17 +0700

    saya sudah mengerjakan tugas 2

---
+++ input_view_anggota.php
@@ -0,0 +1,37 @@
+<?php
+include("koneksi.php");
+
+if(isset($_POST['save'])){
+$query_input=mysqli_query($koneksi,"insert into anggota(nama,alamat,no_tlpn)
+values(
+'".$_POST['nama']."',
+'".$_POST['alamat']."',
+'".$_POST['no_tlpn']."')");
+
+if($query_input){
+header('location:view_anggota.php');
+}else{
+	echo mysqli_error();
+}
+}
+?>
+<form method="POST">
+<table class="table table-bordered" border="1">
+	<tr>
+		<td>Nama</td>
+		<td><input type="text" name="nama" class="form-control"></td>
+	</tr>
+	<tr>
+		<td>Alamat</td>
+		<td><input type="text" name="alamat" class="form-control"></td>
+	</tr>
+	<tr>
+		<td>No Tlpn</td>
+		<td><input type="text" name="no_tlpn" class="form-control"></td>
+	</tr>
+		<tr>
+			<td><input type="submit" name="save" class="btn btn-danger"></td>
+			<td><input type="reset" name="reset"></td>
+		</tr>
+</table>
+</form>
--- view_anggota.php
+++ view_anggota.php
@@ -5,19 +5,19 @@ $query_view=mysqli_query($koneksi, "select * from anggota");
 
 ?>
 
-<table class="table table-bordered" border="1">
+<table width="400" border="1">
     <tr>
-        <td>No</td>
-        <td>Nama</td>
-        <td>Alamat</td>
-        <td>No Tlpn</td>
+        <td align='center'>No</td>
+        <td align='center'>Nama</td>
+        <td align='center'>Alamat</td>
+        <td align='center'>No Tlpn</td>
     </tr>
 
 <?php
 $no=1;
 while ($tampil=mysqli_fetch_array($query_view)) { ?>
     <tr>
-        <td><?php echo $no++;?></td>
+        <td align='center'><?php echo $no++;?></td>
         <td><?php echo $tampil['nama'];?></td>
         <td><?php echo $tampil[ 'alamat'];?></td>
         <td><?php echo $tampil['no_tlpn'];?></td>
